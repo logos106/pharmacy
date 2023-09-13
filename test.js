8eb9ad156e39609a0c854a55eb64a8aec13c783a
Logos106 <logos106@outlook.com>
2023-09-14 03:47:02 +0800

    register

--- test.js
+++ test.js
@@ -1,24 +1,8 @@
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
 
-const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "nxwang00@gmail.com",
-        pass: "suycpzjcuopcrjkl"
-    }
+const token = jwt.sign({ sub: 'user.id', email: 'user.email' }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
 });
 
-var mailOptions = {
-    from: '"Example Team" <nxwang00@gmail.com',
-    to: 'logos106@outlook.com',
-    subject: 'Nice Nodemailer test',
-    text: 'Hey there, it’s our first message sent with Nodemailer 😉 ',
-    html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer'
-};
-
-transport.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-});
+console.log(token)
