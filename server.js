4ab5cef6f53c8dc0249f4ba13a36768a1821d209
Logos106 <logos106@outlook.com>
2023-09-14 01:52:08 +0800

    register

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(cors(corsOptions))
 router.use('/user', require('./routes/user.routes'));
 app.use("/api", router);
 
-const port = 4000; //process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
+const port = 5000; //process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
 app.listen(port, () => console.log('Pharmacy backend is listening on port' + port));
