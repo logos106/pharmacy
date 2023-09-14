75cce03432b1bbcdca71221a6cbe43cd9666c5ed
Logos106 <logos106@outlook.com>
2023-09-14 19:44:37 +0000

    d

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
-const token = jwt.sign({ sub: 'user.id', email: 'user.email' }, process.env.JWT_SECRET, {
+const token = jwt.sign({ sub: 'user.id', email: 'user.email' }, 'hellosecret', {
     expiresIn: '7d',
 });
 
