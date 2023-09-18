98ccc469b89b307617caf6e124ec4735bde5177d
Logos106 <logos106@outlook.com>
2023-09-19 02:11:38 +0800

    aa

--- routes/v1/auth.route.js
+++ routes/v1/auth.route.js
@@ -4,7 +4,7 @@ const authValidation = require('../../validations/auth.validation');
 const authController = require('../../controllers/auth.controller');
 
 router.post('/register', authValidation.register, authController.register);
-router.post('/send-verification-email', authController.sendVerificationEmail);
+router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
 router.post('/verify-email', authValidation.verifyEmail, authController.verifyEmail);
 
 module.exports = router;
