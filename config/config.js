84019840daed8b951a846639d110c5262d5edc5d
Logos106 <logos106@outlook.com>
2023-09-19 02:01:44 +0800

    aa

---
+++ config/config.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('joi');
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    PORT: Joi.number().default(5000),
+    JWT_SECRET: Joi.string().required().description('JWT secret key')
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  jwt: {
+    secret: envVars.JWT_SECRET
+  },
+  email: {
+    service: "gmail",
+    auth: {
+        user: "nxwang00@gmail.com",
+        pass: "suycpzjcuopcrjkl"
+    }
+  }
+
+};
