9d204a65ba49927f586995092834feac0a412d61
Logos106 <logos106@outlook.com>
2023-09-19 10:03:36 +0800

    send email

--- pharmacy.sql
+++ pharmacy.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
+-- HeidiSQL Version:             12.5.0.6677
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -21,11 +21,12 @@ CREATE TABLE IF NOT EXISTS `sequelizemeta` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table pharmacy.sequelizemeta: ~1 rows (approximately)
+-- Dumping data for table pharmacy.sequelizemeta: ~4 rows (approximately)
 INSERT INTO `sequelizemeta` (`name`) VALUES
 	('20230905165706-create-user.js'),
 	('20230905171441-alter-user.js'),
-	('20230907172154-create-token.js');
+	('20230907172154-create-token.js'),
+	('20230915153230-alter-token.js');
 
 -- Dumping structure for table pharmacy.tokens
 CREATE TABLE IF NOT EXISTS `tokens` (
@@ -34,10 +35,16 @@ CREATE TABLE IF NOT EXISTS `tokens` (
   `token` varchar(255) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
+  `type` enum('access','refresh','resetPassword','verifyemail') DEFAULT NULL,
+  `expires` datetime DEFAULT NULL,
+  `blacklisted` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table pharmacy.tokens: ~0 rows (approximately)
+-- Dumping data for table pharmacy.tokens: ~2 rows (approximately)
+INSERT INTO `tokens` (`id`, `userID`, `token`, `createdAt`, `updatedAt`, `type`, `expires`, `blacklisted`) VALUES
+	(1, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyLCJpYXQiOjE2OTUwNTg2MzIsImV4cCI6MTY5NTA1ODYzMiwidHlwZSI6InJlZnJlc2gifQ.VjjW6g-APxG4UOoQx4wgD3RV4u1e_l3cGwDR8CI9Zqo', '2023-09-18 17:37:12', '2023-09-18 17:37:12', 'refresh', NULL, NULL),
+	(2, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTUwODgxNzYsImV4cCI6MTY5NTA4ODE3NiwidHlwZSI6InZlcmlmeUVtYWlsIn0.mFUohTrbPTzFBdd4deB6vgaWO9jmm4BBrhrJPw-oaMQ', '2023-09-19 01:49:36', '2023-09-19 01:49:36', '', NULL, NULL);
 
 -- Dumping structure for table pharmacy.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -52,12 +59,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `token` varchar(500) DEFAULT NULL,
   `isActive` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table pharmacy.users: ~2 rows (approximately)
+-- Dumping data for table pharmacy.users: ~1 rows (approximately)
 INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `createdAt`, `updatedAt`, `password`, `role`, `token`, `isActive`) VALUES
-	(1, 'Seok', 'Joe', 'logos106@outlook.com', '2023-09-06 17:00:02', '2023-09-12 10:54:04', '$2a$10$ous4HlcEmx5QqKDy53yhLex6lLkZ8lOEIUtS1sY/RgTFgA1Rt74/m', 'user', '', 1),
-	(2, 'Seok', 'Joe', 'logos821@outlook.com', '2023-09-07 17:43:28', '2023-09-07 17:43:28', '$2a$10$bUebnhduPj.tfR.l9968eOZZZb/jzX0p8fdZTFpJe5HZ9tmV9y.kO', 'user', NULL, 0);
+	(12, 'Seok', 'Joe', 'kimyoonsoo111@outlook.com', '2023-09-18 17:37:12', '2023-09-18 17:37:12', 'glowGlow1', 'user', NULL, 0);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
