ab682298240256d6238dc524ab36d9b32e6659af
Logos106 <logos106@outlook.com>
2023-09-21 02:15:09 +0800

    B

--- server/routes/v1/auth.route.js
+++ server/routes/v1/auth.route.js
@@ -12,7 +12,5 @@ router.get('/forgot-email', authController.forgotEmail);
 router.post('/reset-password', authValidation.resetPassword, authController.resetPassword);
 router.post('/login', authValidation.login, authController.login);
 router.post('/logout', authValidation.logout, authController.logout);
-
-// Joe is trying to deploy  
-
+//
 module.exports = router;
