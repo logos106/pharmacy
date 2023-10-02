681a46b6bf7686af15f5306c3219174fddf40822
Logos106 <logos106@outlook.com>
2023-10-02 17:00:59 +0800

    a

--- test.js
+++ test.js
@@ -1,2 +1,24 @@
 
-console.log(__dirname)
+nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp2go.com',
+    port: 2525,
+    auth: {
+        user: 'nxwang',
+        pass: '5DFCT8sVyn32FVg9'
+    }
+});
+
+async function main() {
+    // send email
+    await transporter.sendMail({
+        from: 'logos106@outlook.com',
+        to: 'nxwang00@gmail.com',
+        subject: 'Test Email Subject',
+        html: '<h1>Example HTML Message Body</h1>'
+    });
+}
+
+main().catch(console.error);
+
